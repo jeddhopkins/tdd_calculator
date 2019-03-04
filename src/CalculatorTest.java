fa51240a65f71fa3568817cf46789dd8bbcc46c1
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-03-04 20:28:28 +0000

    first commit

---
+++ src/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest extends Calculator {
+	
+	@DisplayName("can add two numbers together")
+	@Test
+	
+	
+}
