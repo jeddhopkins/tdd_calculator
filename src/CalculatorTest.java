54d1302ae2336522b1b58c297d29a13feaef22d6
Jedd Hopkins <jedd_hoppo@hotmail.com>
2019-03-04 21:31:55 +0000

    add subtract multiply divide functions

--- src/CalculatorTest.java
+++ src/CalculatorTest.java
@@ -1,12 +1,129 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest extends Calculator {
 	
-	@DisplayName("can add two numbers together")
-	@Test
+	public Calculator calc;
+	
+	@BeforeEach
+	public void before() {
+		calc = new Calculator();
+	}
+	
+	@Nested
+	@DisplayName("adding two numbers")
+	class Adding {
+		
+		@DisplayName("3 + 3 should equal 6")
+		@Test
+		void threeThree() {
+			assertEquals(6, calc.add(3, 3));
+		}
+		
+		@DisplayName("(-4) + 3 should equal -1")
+		@Test
+		void fourThree() {
+			assertEquals(-1, calc.add(-4, 3));
+		}
+		
+		@DisplayName("3 + (-4) should equal -1")
+		@Test
+		void threeFour() {
+			assertEquals(-1, calc.add(3, -4));
+		}
+
+//		@DisplayName("2.5 + 2.6 equals 5.1")
+//		@Test
+//		void decimalDecimal() {
+//			assertEquals(5.1, calc.add(2.5, 2.6));
+//		}
+	}
+	
+	@Nested
+	@DisplayName("subtracting two numbers")
+	class Subtracting {
+		
+		@DisplayName("8 - 4 equals 4")
+		@Test
+		void eightFour() {
+			assertEquals(4, calc.subtract(8, 4));
+		}
+		
+		@DisplayName("10 - (-2) equals 12")
+		@Test
+		void tenTwo() {
+			assertEquals(12, calc.subtract(10, -2));
+		}
+		
+		@DisplayName("-3 - 5 equals -8")
+		@Test
+		void threeFive() {
+			assertEquals(-8, calc.subtract(-3, 5));
+		}
+	}
+	
+	@Nested
+	@DisplayName("multiplying two numbers")
+	class Multiplying {
+		
+		@DisplayName("5 * 5 equals 25")
+		@Test
+		void fiveFive() {
+			assertEquals(25, calc.multiply(5, 5));
+		}
+		
+		@DisplayName("5 * (-6) equals -30")
+		@Test
+		void fivesix() {
+			assertEquals(-30, calc.multiply(5, -6));
+		}
+		
+		@DisplayName("(-5) * 5 equals -25")
+		@Test
+		void minusfiveFive() {
+			assertEquals(-25, calc.multiply(-5, 5));
+		}
+	}
+	
+	@Nested
+	@DisplayName("dividing two numbers")
+	class Divide {
+		
+		@DisplayName("10 / 5 equals 2")
+		@Test
+		void tenTwo() {
+			assertEquals(2, calc.divide(10, 5));
+		}
+		
+		@DisplayName("(-10) / 5 equals -2")
+		@Test
+		void minustenfive() {
+			assertEquals(-2, calc.divide(-10, 5));
+		}
+		
+		@DisplayName("10 / (-2) equals -5")
+		@Test
+		void tenminusTwo() {
+			assertEquals(5, calc.divide(10, 2));
+		}
+		
+		@DisplayName("10 / 0 throws an error")
+		@Test
+		void tenzero() {
+			
+		}
+	}
+	
+	@Nested
+	@DisplayName("square of a number")
+	class Square {
+		
+		@DisplayName("")
+	}
 	
 	
 }
